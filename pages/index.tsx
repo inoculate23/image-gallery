c93a45a61a2c9606d1db15844d90f0d2c6300816
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-19 15:12:00 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,7 +68,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
-               href="mailto:craig@inoculate.media, terbo@inoculate.media">
+               href="mailto:craig@inoculate.media, terbo@inoculate.media"
   
 
               target="_blank"
