7b469fdb74c7d4f252938b5c793980009b887662
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-19 16:53:36 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,15 +30,27 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   return (
     <>
       <Head>
-        <title>Haawke Neural - Las Vegas design & consulting</title>
-        <meta
-          property="og:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
-        />
-        <meta
-          name="twitter:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
-        />
+     <head>
+       <!-- Primary Meta Tags -->
+<title>Haawke Neural Technology - Las Vegas AI and machine learning experts</title>
+<meta name="title" content="Haawke Neural Technology - Las Vegas AI and machine learning experts" />
+<meta name="description" content="Haawke Neural - AI and machine learning design and consulting agency in Las Vegas, Nevada. Our emphasis is on creating new technology to enhance visitors' experiences in Las Vegas, and beyond." />
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://metatags.io" />
+<meta property="og:title" content="Haawke Neural Technology - Las Vegas AI and machine learning experts" />
+<meta property="og:description" content="Haawke Neural - AI and machine learning design and consulting agency in Las Vegas, Nevada. Our emphasis is on creating new technology to enhance visitors' experiences in Las Vegas, and beyond." />
+<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="https://metatags.io" />
+<meta property="twitter:title" content="Haawke Neural Technology - Las Vegas AI and machine learning experts" />
+<meta property="twitter:description" content="Haawke Neural - AI and machine learning design and consulting agency in Las Vegas, Nevada. Our emphasis is on creating new technology to enhance visitors' experiences in Las Vegas, and beyond." />
+<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+<!-- Meta Tags Generated with https://metatags.io -->
       </Head>
       <main className="mx-auto max-w-[1960px] p-4">
         {photoId && (
