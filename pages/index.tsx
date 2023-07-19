fc3d6450bf009e1c64b6e6d3bfc68104d970105a
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-19 15:42:51 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -105,12 +105,13 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         </div>
       </main>
       <footer className="p-6 text-center text-white/80 sm:p-12">
-Inoculate Media - "Design, Coding. & Consulting"  702=776-1124  Las Vegas, NV
-    All rights reserved, 2023
+ All rights reserved, 2023 - Inoculate Media - "Design, Coding. & Consulting"  702-776-1124  Las Vegas, NV
+   
       </footer>
     </>
   )
 }
+
 
 export default Home
 
