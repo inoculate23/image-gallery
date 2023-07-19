9212c37f09a1324b0d408aabac1ef5740eb19f01
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-19 16:57:40 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,14 +15,14 @@ class MyDocument extends Document {
 <meta property="og:url" content="https://metatags.io" />
 <meta property="og:title" content="Haawke Neural Technology - Las Vegas AI and machine learning experts" />
 <meta property="og:description" content="Haawke Neural - AI and machine learning design and consulting agency in Las Vegas, Nevada. Our emphasis is on creating new technology to enhance visitors' experiences in Las Vegas, and beyond." />
-<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+<meta property="og:image" content="Haawke Neural Technology - Las Vegas AI and machine lea
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:url" content="https://metatags.io" />
 <meta property="twitter:title" content="Haawke Neural Technology - Las Vegas AI and machine learning experts" />
 <meta property="twitter:description" content="Haawke Neural - AI and machine learning design and consulting agency in Las Vegas, Nevada. Our emphasis is on creating new technology to enhance visitors' experiences in Las Vegas, and beyond." />
-<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+<meta property="twitter:image" content="Haawke Neural Technology - Las Vegas AI and machine lea
 
 <!-- Meta Tags Generated with https://metatags.io -->
         </Head>
