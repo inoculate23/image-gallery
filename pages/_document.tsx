b9fece7dbe9ba3b1865272ec0571d8cc94000a91
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-21 06:17:38 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,26 +5,26 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <head>
-       <!-- Primary Meta Tags -->
+  
 <title>Haawke Neural Technology - Las Vegas AI and machine learning experts</title>
 <meta name="title" content="Haawke Neural Technology - Las Vegas AI and machine learning experts" />
 <meta name="description" content="Haawke Neural - AI and machine learning design and consulting agency in Las Vegas, Nevada. Our emphasis is on creating new technology to enhance visitors' experiences in Las Vegas, and beyond." />
 
-<!-- Open Graph / Facebook -->
+
 <meta property="og:type" content="website" />
 <meta property="og:url" content="https://metatags.io" />
 <meta property="og:title" content="Haawke Neural Technology - Las Vegas AI and machine learning experts" />
 <meta property="og:description" content="Haawke Neural - AI and machine learning design and consulting agency in Las Vegas, Nevada. Our emphasis is on creating new technology to enhance visitors' experiences in Las Vegas, and beyond." />
 <meta property="og:image" content="Haawke Neural Technology - Las Vegas AI and machine lea
 
-<!-- Twitter -->
+
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:url" content="https://metatags.io" />
 <meta property="twitter:title" content="Haawke Neural Technology - Las Vegas AI and machine learning experts" />
 <meta property="twitter:description" content="Haawke Neural - AI and machine learning design and consulting agency in Las Vegas, Nevada. Our emphasis is on creating new technology to enhance visitors' experiences in Las Vegas, and beyond." />
 <meta property="twitter:image" content="Haawke Neural Technology - Las Vegas AI and machine lea
 
-<!-- Meta Tags Generated with https://metatags.io -->
+
         </Head>
         <body className="bg-black antialiased">
           <Main />
