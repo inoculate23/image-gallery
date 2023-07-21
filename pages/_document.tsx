f3378184534efe2fb8eff0d7665653def3cc1629
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-21 06:35:34 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,22 +7,6 @@ class MyDocument extends Document {
         <head>
   
 <title>Haawke Neural Technology - Las Vegas AI and machine learning experts</title>
-<meta name="title" content="Haawke Neural Technology - Las Vegas AI and machine learning experts" />
-<meta name="description" content="Haawke Neural - AI and machine learning design and consulting agency in Las Vegas, Nevada. Our emphasis is on creating new technology to enhance visitors' experiences in Las Vegas, and beyond." />
-
-
-<meta property="og:type" content="website" />
-<meta property="og:url" content="https://metatags.io" />
-<meta property="og:title" content="Haawke Neural Technology - Las Vegas AI and machine learning experts" />
-<meta property="og:description" content="Haawke Neural - AI and machine learning design and consulting agency in Las Vegas, Nevada. Our emphasis is on creating new technology to enhance visitors' experiences in Las Vegas, and beyond." />
-<meta property="og:image" content="Haawke Neural Technology - Las Vegas AI and machine lea
-
-
-<meta property="twitter:card" content="summary_large_image" />
-<meta property="twitter:url" content="https://metatags.io" />
-<meta property="twitter:title" content="Haawke Neural Technology - Las Vegas AI and machine learning experts" />
-<meta property="twitter:description" content="Haawke Neural - AI and machine learning design and consulting agency in Las Vegas, Nevada. Our emphasis is on creating new technology to enhance visitors' experiences in Las Vegas, and beyond." />
-<meta property="twitter:image" content="Haawke Neural Technology - Las Vegas AI and machine lea
 
 
         </Head>
