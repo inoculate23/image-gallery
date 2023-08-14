aee5bbeea925fb3c0ed385cc8245ad51099fcfc2
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-08-14 12:19:23 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               </span>
               <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
             </div>
-            <Logo />
+ 
             <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
             Haawke Neural by Inoculate Media
               
