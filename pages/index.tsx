adc9ca76e4a88ef4e53d7a64c47d9c3e7d3f1ee8
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-08-14 12:04:14 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,11 +26,19 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   }, [photoId, lastViewedPhoto, setLastViewedPhoto])
 
   return (
-  
-    
-     <head>
-
-      </head>
+   return (
+    <>
+      <Head>
+        <title>Haawke Neural - Las Vegas design & consulting</title>
+        <meta
+          property="og:image"
+          content="https://nextjsconf-pics.vercel.app/og-image.png"
+        />
+        <meta
+          name="twitter:image"
+          content="https://nextjsconf-pics.vercel.app/og-image.png"
+        />
+      </Head>
       <main className="mx-auto max-w-[1960px] p-4">
         {photoId && (
           <Modal
